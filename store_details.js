ddc81207a9cb9e9ebe1e4cd16bf3ea739244bc26
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-29 12:56:40 -0500

    Twin Pines Mall 1.0  /store_details.js

--- store_details.js
+++ store_details.js
@@ -177,7 +177,7 @@
                 });
             }else if (type == "hours"){
                 hours = getHoursForIds(collection.store_hours)
-                console.log(val.hours)
+                console.log(hours)
                 $.each( hours , function( key, val ) {
                     switch(val.day_of_week) {
                         case 0:
