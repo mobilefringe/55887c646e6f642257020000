198a228b47c98f50001dc38dc9f7fd9e081b87d1
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-01-29 11:55:18 -0500

    Twin Pines Mall 1.0  /application.js

--- application.js
+++ application.js
@@ -37,7 +37,8 @@ function renderHomeBlog(container, template, collection){
         }
         val.description_short = val.description_short.replace("&amp;", "&");
         var date_blog = new Date((val.publish_date + " 05:00:00").replace(/-/g,"/"));
-        val.published_day = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
+        val.published_day = date_blog.getDate();
+        val.published_month = get_month(date_blog.getMonth()) ;
         
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
